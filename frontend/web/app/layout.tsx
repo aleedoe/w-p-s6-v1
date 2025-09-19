b4fd08bf2fa270126b9bc72fb5967ad229525d63
aleedoe <mushyafaali@gmail.com>
2025-09-19 16:39:36 +0700

    create dashboard layout component

--- frontend/web/app/layout.tsx
+++ frontend/web/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          "min-h-screen text-foreground bg-background font-sans antialiased",
+          "font-sans antialiased",
           fontSans.variable,
         )}
       >
